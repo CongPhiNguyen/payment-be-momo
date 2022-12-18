e26b3e1aaf1f12d0f53330847babe9226065299b
CongPhiNguyen <congphinguyen312@gmail.com>
2022-12-18 20:02:47 +0700

    be-payment

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+var cookieParser = require("cookie-parser")
+const route = require("./routes")
+
+const app = express()
+
+app.use(cookieParser())
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*"
+  })
+)
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+route(app)
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
